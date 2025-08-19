ab4f7bfe6d3bbb32975110aacf3000e79b3a81ae
Vj <mabansagbj@gmail.com>
2025-08-19 12:08:50 +0800

    Refactor initAccordion function to simplify event listener attachment

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,34 +1,18 @@
-/*function initAccordion()
-{
-    const triggers = document.querySelectorAll('.faq__trigger');
-
-    triggers.forEach((btn) => {
-        btn.addEventListener('click', () => {
-            const panelId = btn.getAttribute('aria-controls');
-            const panel = panelId ? document.getElementById(panelId) : null;
-
-            const isOpen = btn.getAttribute('aria-expanded') === 'true';
-            const nextState = !isOpen;
-
-            btn.setAttribute('aria-expanded', nextState);
-
-            if (panel) 
-                panel.hidden = !nextState;
-        });
-
-    });
-}*/
 function initAccordion()
 {
     const triggers = document.querySelectorAll('faq__trigger');
 
     for(var i = 0; i < triggers.length; i++)
     {
-        var btn = triggers[i];
-
-        btn.addEventListener('click', function(event)
+        triggers[i].addEventListener('click', function(event)
         {
-            var btn = 
+            var isOpen = this.getAttribute('aria-expanded') === 'true';
+            var panelId = this.getAttribute('aria-controls');
+            var panel = panelId ? document.getElementById(panelId) : null;
+
+            this.setAttribute('aria-expanded', !isOpen);
+            if (panel)
+                panel.hidden = !isOpen;
         }
         )
     }
